b3a2f778874c9af40b131cd2796d2b08c418a41e
EvanilsonP <pevanilson99@gmail.com>
2023-05-09 11:09:12 -0300

    creating delete task function

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ function addTask() {
     // clearing input field after clicking the button
     inputBox.value = '';
 }
+
+listContainer.addEventListener('click', function(e) {
+    if(e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+    } else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+}, false);
