d4ac2258dc99d8c717ace392f0677dd47976c158
EvanilsonP <pevanilson99@gmail.com>
2023-05-09 11:03:48 -0300

    adding css

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
 function addTask() {
+    // In case you click to add a task without providing any information on it:
     if(inputBox.value === '') {
         alert('You must write something!');
     } else {
         let li = document.createElement('li');
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
     }
+    // clearing input field after clicking the button
+    inputBox.value = '';
 }
